0ba195d017d384a01101d2b5597c30cc81f1069d
Matheus Harsani <Matheus.Harsani@keyruscorp.key>
2018-09-05 11:11:47 -0300

    feature pagination

--- src/pagination/pagination.test.js
+++ src/pagination/pagination.test.js
@@ -29,3 +29,43 @@ test('pagination({total: 6, activePage: 1}) should return [1, 2, 3, "...", 6]', 
     expect(pagination(params)).to.be.deep.equal(result)
 
 })
+test('pagination({total: 6, activePage: 2}) should return [1, 2, 3, "...", 6]', () => {
+    const params = {total: 6, activePage: 2}
+    const result =  [1, 2, 3, '...', 6]
+    expect(pagination(params)).to.be.deep.equal(result)
+
+})
+test('pagination({total: 6, activePage: 3}) should return [1, 2, 3, 4, 5, 6]', () => {
+    const params = {total: 6, activePage: 3}
+    const result =  [1, 2, 3, 4, 5, 6]
+    expect(pagination(params)).to.be.deep.equal(result)
+
+})
+
+test('pagination({total: 6, activePage: 4}) should return [1, 2, 3, 4, 5, 6]', () => {
+    const params = {total: 6, activePage: 4}
+    const result =  [1, 2, 3, 4, 5, 6]
+    expect(pagination(params)).to.be.deep.equal(result)
+
+})
+
+test('pagination({total: 6, activePage: 5}) should return [1, "...", 4, 5, 6]', () => {
+    const params = {total: 6, activePage: 5}
+    const result =  [1, '...', 4, 5, 6]
+    expect(pagination(params)).to.be.deep.equal(result)
+
+})
+test('pagination({total: 6, activePage: 6}) should return [1, "...", 4, 5, 6]', () => {
+    const params = {total: 6, activePage: 6}
+    const result =  [1, '...', 4, 5, 6]
+    expect(pagination(params)).to.be.deep.equal(result)
+
+})
+
+test('pagination({total: 7, activePage: 6}) should return [1, "...", 4, 5, 6]', () => {
+    const params = {total: 6, activePage: 6}
+    const result =  [1, '...', 4, 5, 6]
+    expect(pagination(params)).to.be.deep.equal(result)
+
+})
+
